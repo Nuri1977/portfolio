79a4a184e079e55f5eac0e8a9ec30f5d66cface6
Nuri1977 <nurilacka@gmail.com>
2022-06-23 12:38:40 +0200

    Add text typing animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Intro.scss';
+import { init } from 'ityped';
 
 const Intro = () => {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      strings: ["Developer"],
+    });
+  }, []);
+
   return (
     <div className='intro' id="intro">
       <div className="left">
@@ -13,7 +25,7 @@ const Intro = () => {
         <div className="wrapper">
           <h2>Hi There, I'm </h2>
           <h1>Nuri Lacka</h1>
-          <h3>Full-stack <span>developer</span></h3>
+          <h3>Full-stack <span ref={textRef}></span></h3>
         </div>
         <a href="#portfolio">
           <img src="assets/down.png" alt="" />
