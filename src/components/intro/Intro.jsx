7a8731cfe01f6e9ad82fcbdfbd3cdd612e564d60
Nuri1977 <nurilacka@gmail.com>
2022-06-23 12:09:17 +0200

    Add home section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,23 @@ import './Intro.scss';
 
 const Intro = () => {
   return (
-    <div className='intro' id="intro"></div>
+    <div className='intro' id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm </h2>
+          <h1>Nuri Lacka</h1>
+          <h3>Full-stack <span>developer</span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
   )
 }
 
