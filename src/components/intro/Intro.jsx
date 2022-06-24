b6098149d9ed6ee20d93abbdbb29dccfc56fffac
Nuri1977 <nurilacka@gmail.com>
2022-06-24 12:33:46 +0200

    Add my picture to inro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ const Intro = () => {
     <div className='intro' id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/man.png" alt="" />
+          <img src="assets/nuri.jpg" alt="" />
         </div>
       </div>
       <div className="right">
