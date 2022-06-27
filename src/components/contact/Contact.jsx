051f34bb376041de0ef18e9c79cb4012d9d3e4e2
Nuri1977 <nurilacka@gmail.com>
2022-06-27 20:00:44 +0200

    Add formspree for contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,62 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 import "./Contact.scss";
 
 const Contact = () => {
-  const [message, setMessage] = useState(false);
+  const formData = useRef();
+  const [state, handleSubmit] = useForm("xqknzyyj");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setMessage(true);
-  };
+  if (state.succeeded) {
+    formData.current.reset();
+      return (
+        <div className="contact" id="contact">
+          <div className="left">
+            <img src="assets/shake.svg" alt="" />
+          </div>
+          <div className="right">
+            <h2>Contact.</h2>
+            {/* <form onSubmit={handleSubmit}>
+              <input type="text" placeholder="Email" />
+              <textarea placeholder="Message"></textarea>
+              <button type="submit">Send</button>
+              {message && <span>Thanks, I'll reply ASAP </span>}
+            </form> */}
+        <form onSubmit={handleSubmit} ref={formData}>
+          <label htmlFor="email">
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+          />
+          <ValidationError
+            prefix="Email"
+            field="email"
+            errors={state.errors}
+          />
+          <textarea
+            id="message"
+            name="message"
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+          <button type="submit" disabled={state.submitting}>
+            Submit
+          </button>
+          <span>Thanks, I'll reply ASAP </span>
+        </form>
+          </div>
+        </div>
+      );
+  }
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setMessage(true);
+  // };
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -15,12 +64,38 @@ const Contact = () => {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
+        {/* <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Email" />
           <textarea placeholder="Message"></textarea>
           <button type="submit">Send</button>
           {message && <span>Thanks, I'll reply ASAP </span>}
-        </form>
+        </form> */}
+    <form onSubmit={handleSubmit} ref={formData}>
+      <label htmlFor="email">
+      </label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+      />
+      <ValidationError
+        prefix="Email"
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError
+        prefix="Message"
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
       </div>
     </div>
   );
