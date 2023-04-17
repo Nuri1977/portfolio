fa4a2dff9d059cdca5d7098355659f06ce5861e9
Nuri1977 <nurilacka@gmail.com>
2023-04-17 13:37:59 +0200

    Change Intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,23 @@
 import React, { useEffect, useRef } from 'react';
 import './Intro.scss';
 import { init } from 'ityped';
-import { SiRubyonrails, SiJavascript, SiReact, SiPostgresql } from "react-icons/si";
+import {
+  SiRubyonrails,
+  SiJavascript,
+  SiReact,
+  SiPostgresql,
+  SiTypescript,
+  SiRedux,
+  SiMui,
+  SiTailwindcss,
+  SiJest,
+  SiTestinglibrary,
+  SiCypress,
+  SiFirebase,
+  SiApollographql,
+  SiSwagger,
+  SiDocker,
+} from 'react-icons/si';
 
 const Intro = () => {
   const textRef = useRef();
@@ -10,39 +26,53 @@ const Intro = () => {
     init(textRef.current, {
       showCursor: true,
       backDelay: 1500,
-      backSpeed:60,
-      strings: ["Developer"],
+      backSpeed: 60,
+      strings: ['Developer'],
     });
   }, []);
 
   return (
-    <div className='intro' id="intro">
-      <div className="left">
-        <div className="imgContainer">
-          <img src="assets/nuri.jpg" alt="" />
-          <div>
-            <h3>Stack: JavaScript, React, Rails</h3>
+    <div className='intro' id='intro'>
+      <div className='left'>
+        <div className='imgContainer'>
+          <div className='social'>
+            <SiJavascript className='icons' />
+            <SiTypescript className='icons' />
+            <SiReact className='icons' />
+            <SiRedux className='icons' />
+            <SiMui className='icons' />
+            <SiTailwindcss className='icons' />
           </div>
           <div className='social'>
-            <SiRubyonrails className='icons'/>
-            <SiJavascript className='icons'/>
-            <SiReact className='icons'/>
-            <SiPostgresql className='icons'/>
+            <SiJest className='icons' />
+            <SiTestinglibrary className='icons' />
+            <SiCypress className='icons' />
+            <SiApollographql className='icons' />
+            <SiSwagger className='icons' />
+          </div>
+          <div className='social'>
+            <SiRubyonrails className='icons' />
+            <SiPostgresql className='icons' />
+            <SiFirebase className='icons' />
+            <SiDocker className='icons' />
           </div>
         </div>
       </div>
-      <div className="right">
-        <div className="wrapper">
+      <div className='right'>
+        <div className='wrapper'>
+          <img src='assets/nuri.jpg' alt='' />
           <h2>Hi There, I'm </h2>
           <h1>Nuri Lacka</h1>
-          <h3>Full-stack <span ref={textRef}></span></h3>
+          <h3>
+            Front-end <span ref={textRef}></span>
+          </h3>
         </div>
-        <a href="#portfolio">
-          <img src="assets/down.png" alt="" />
+        <a href='#portfolio'>
+          <img src='assets/down.png' alt='' />
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,28 +1,49 @@
 import React from 'react';
 import './Topbar.scss';
-import { FaTwitterSquare, FaGithub, FaLinkedin} from 'react-icons/fa';
+import { FaTwitterSquare, FaGithub, FaLinkedin } from 'react-icons/fa';
 
-const Topbar = ({menuOpen, setMenuOpen}) => {
-
-
+const Topbar = ({ menuOpen, setMenuOpen }) => {
   return (
-    <div className={"topbar " + (menuOpen && "active")} >
-      <div className="wrapper">
-        <div className="left">
-          <a href="#intro" className='logo'>Logo.</a>
-          <div className="itemContainer">
-            <a href="https://github.com/Nuri1977" target='_blank' rel="noreferrer" className='icon'><FaGithub className='icon'/></a>
+    <div className={'topbar ' + (menuOpen && 'active')}>
+      <div className='wrapper'>
+        <div className='left'>
+          <a href='#intro' className='logo'>
+            Logo.
+          </a>
+          <div className='itemContainer'>
+            <a
+              href='https://github.com/Nuri1977'
+              target='_blank'
+              rel='noreferrer'
+              className='icon'
+            >
+              <FaGithub className='icon' />
+            </a>
           </div>
-          <div className="itemContainer">
-            <a href="https://www.linkedin.com/in/nuri-lacka/" target='_blank' rel="noreferrer" className='icon'><FaLinkedin className='icon'/></a>
+          <div className='itemContainer'>
+            <a
+              href='https://www.linkedin.com/in/nuri-lacka/'
+              target='_blank'
+              rel='noreferrer'
+              className='icon'
+            >
+              <FaLinkedin className='icon' />
+            </a>
           </div>
-          <div className="itemContainer">
-            <a href="https://twitter.com/LackaNuri" target='_blank' rel="noreferrer" className='icon'><FaTwitterSquare className='icon'/></a>
+          <div className='itemContainer'>
+            <a
+              href='https://twitter.com/LackaNuri'
+              target='_blank'
+              rel='noreferrer'
+              className='icon'
+            >
+              <FaTwitterSquare className='icon' />
+            </a>
           </div>
         </div>
 
-        <div className="right">
-          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <div className='right'>
+          <div className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
             <span className='line1'></span>
             <span className='line2'></span>
             <span className='line3'></span>
@@ -30,7 +51,7 @@ const Topbar = ({menuOpen, setMenuOpen}) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;
